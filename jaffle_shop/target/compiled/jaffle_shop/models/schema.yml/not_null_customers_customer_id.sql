67f02c6ca616127e05850419fff7e3bec687d3d1
Deepa Balwally <deepa.balwally@hevodata.com>
2022-11-14 13:03:54 +0000

    Run files

---
+++ jaffle_shop/target/compiled/jaffle_shop/models/schema.yml/not_null_customers_customer_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select customer_id
+from `dbt-learn-bq-user`.`jaffle_shop`.`customers`
+where customer_id is null
+
+
---
+++ jaffle_shop/target/compiled/jaffle_shop/models/schema.yml/not_null_stg_orders_customer_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select customer_id
+from `dbt-learn-bq-user`.`jaffle_shop`.`stg_orders`
+where customer_id is null
+
+
